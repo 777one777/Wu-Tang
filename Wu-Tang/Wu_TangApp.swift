788c9ebb0f7d6b135c7ea0e962b01ba1032d2cb8
777one777 <jcuevavasquez2025@gmail.com>
2025-05-19 15:15:05 -0700

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by Student2 on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
